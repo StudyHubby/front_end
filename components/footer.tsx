e2bcd63e93f127bd9de54b058171dd5240f51745
KYUTI3 <108830682+KYUTI3@users.noreply.github.com>
2025-04-12 20:15:23 -0700

    small update

---
+++ components/footer.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export function footer() {
+ return(
+    <div className="w-full py-4 px-8 space-y-4 text-green-600 text-center text-sm "> 
+
+         <div> Made with Caffeine, Fumes, and Flint and Steel! </div>
+
+         <div> © 2025 StudyHubby | All Rights Reserved </div>
+
+     </div >
+
+        
+    
+    )
+}
+
