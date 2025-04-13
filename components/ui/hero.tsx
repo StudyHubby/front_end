915c89a27dc33501347521b86c6b4a386b81f974
KYUTI3 <108830682+KYUTI3@users.noreply.github.com>
2025-04-13 03:59:17 -0700

    landing page (75% complete)

--- components/ui/hero.tsx
+++ components/ui/hero.tsx
@@ -3,20 +3,20 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div className="w-full py-14 px-14">
-      <div className="max-w-2xl space-y-10 text-lef bg-gray-900">
-        <div className="text-3xl font-bold text-emerald-500">StudyHubby</div>
-     
-        <div className="text-white underline-offset-2">
+    <div className="w-full flex justify-start px-6 py-12">
+      <div className="bg-gray-900 text-white rounded-xl shadow-lg px-10 py-12 max-w-2xl w-full space-y-10">
+        <div className="text-3xl font-bold text-emerald-500">Your Learning... Amplified!</div>
+
+        <div className="underline underline-offset-1 decoration-wavy">
           Reform your studying today with our study soul-mate by your side!
         </div>
+
         <Link href="/dashboard">
-          <button className="bg-emerald-600 text-white px-6 py-3 rounded-xl hover:bg-emerald-700 transition">
+          <button className="bg-emerald-600 text-white px-6 py-3 rounded-xl hover:bg-emerald-700 transition mt-6">
             Sign Up!
           </button>
         </Link>
-       </div>
       </div>
+    </div>
   )
 }
-
